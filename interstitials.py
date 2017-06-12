ab1ee720875859a78a92556cba7644a1599136a0
Liz Lucas <erl7902@rit.edu>
2017-06-12 16:02:38 -0700

    Added another confidence, another site, and added "ground truth" to list.txt

--- interstitials.py
+++ interstitials.py
@@ -9,6 +9,10 @@ import os
 import argparse
 from itertools import chain
 from collections import Counter
+import re
+from scipy.linalg import norm
+from scipy import sum, average
+import cv2
 
 # Take in filename of list of sites.
 # Run selenium.
@@ -30,16 +34,17 @@ def main():
         images = pullImages(site[1], "screenshots")
         # Now we have a group. Throw that into Hough
         # Bonus: also filters to only include horiz/vertical lines
-        candidates = houghLines(images)     
+        candidates = houghLines(images)
+        diffs = compare_all(images)    
         # From candidates, we want to see the ones that persist       
-        print (site[1] + ": %f" % (getConfidence(candidates)))
+        print (site[1] + ": %f" % (getConfidence(candidates, diffs)))
 
 
 # So we want to find duplicates. 
 # A duplicate is a line that persists over multiple images
 # TODO: there may be some fluctuation on theta & rho values
-def getConfidence(candidates):
-    maxConf = 7
+def getConfidence(candidates, diffs):
+    maxConf = 8
     confidence = 0
     results = []
     #If line appears more than once....keep it) 
@@ -60,18 +65,45 @@ def getConfidence(candidates):
             confidence += 2
         if(len(tightVert) > 1 and len(tightHorz) > 1):
             confidence += 2
+        if(diffs > 2):
+            confidence += 1
     return ((float(confidence)) / (float(maxConf)))
-        
+
+# Assumes in order
+def compare_all(files):
+    results = []
+    images = []
+    for f in files:
+        images.append(cv2.imread(f)[339:539, 535:735])
+    for i in range (1, len(images)):
+        results.append(compare_images(images[i], images[i-1]))
+    return results.count(0)
+
+def compare_images(img1, img2):
+    diff = img1 - img2
+    m_norm = sum(abs(diff))
+    return m_norm
+
 
 # Pull the list of images we need for each part
 # E.g. substr = "Forbes", 
 # we pull [screenshots/ImgForbes01, screenshots/ImgForbes02]
+# Prereq - there are no numbers at the end of the filename except the order
 def pullImages(substr, dirToSearch):
     result = []
     for filename in os.listdir(dirToSearch):
         if substr in filename: 
             result.append(dirToSearch + "/" + filename)
+    result.sort(key = lambda l: grp("\d+\.", l))
     return result
+
+def grp(pat, txt):
+    r = re.search(pat, txt)
+    if r:
+        return float(r.group(0)[:-1])
+    else:
+        return '&'
+
 
 
 if __name__ == "__main__":
